692e3c11c4cdfd35e9550687942b48640a5523b9
xin <xin@qq.com>
2019-05-13 11:42:07 +0800

    web大作业第一次完成进度-user的regist

--- src/main/java/com/it666/room_manager/user/domain/User.java
+++ src/main/java/com/it666/room_manager/user/domain/User.java
@@ -10,4 +10,43 @@ public class User {
     private String email;
     private String code;
 
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
 }
